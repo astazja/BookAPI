034dfe541d88ec24fabb394451dee58e3f2559e8
Katarzyna Pacewicz <katarzyna@pacewi.cz>
2021-05-15 16:23:02 +0200

    init commit

---
+++ src/main/java/cz/pacewi/app/AppConfig.java
@@ -0,0 +1,13 @@
+package cz.pacewi.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "cz.pacewi")
+public class AppConfig implements WebMvcConfigurer {
+
+}
